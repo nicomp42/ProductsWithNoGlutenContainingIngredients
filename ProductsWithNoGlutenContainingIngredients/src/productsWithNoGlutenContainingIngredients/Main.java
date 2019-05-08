1a51882907d14994c63819c705f4dd4b45d66353
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-05-08 09:33:15 -0400

    Second Commit

---
+++ ProductsWithNoGlutenContainingIngredients/src/productsWithNoGlutenContainingIngredients/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+package productsWithNoGlutenContainingIngredients;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ ProductsWithNoGlutenContainingIngredients/src/productsWithNoGlutenContainingIngredients/ProductsWithNoGlutenContainingIngredients.java
@@ -0,0 +1,11 @@
+/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+package productsWithNoGlutenContainingIngredients;
+
+public class ProductsWithNoGlutenContainingIngredients {
+	public static void process(String fileName) {
+		
+	}
+}
